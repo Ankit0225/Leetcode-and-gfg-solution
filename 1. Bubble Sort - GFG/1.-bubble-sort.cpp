1e0a30a8e563ab086abf8a6c042aabf2c5a8449a
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-01-17 23:26:11 +0530

    Added solution - LeetHub

--- 1. Bubble Sort - GFG/1.-bubble-sort.cpp
+++ 1. Bubble Sort - GFG/1.-bubble-sort.cpp
@@ -24,7 +24,7 @@ class Solution
     {
         // Your code here  
         for(int i=0;i<n-1;i++)
-        for(int j=0;j<n-1;j++)
+        for(int j=0;j<n-i-1;j++)
         if(arr[j]>arr[j+1])
         swap(&arr[j],&arr[j+1]);
     }
