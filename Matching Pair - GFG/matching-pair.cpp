078ee9ec44b95d4207420f77bff4d145398c1948
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-01-18 21:56:25 +0530

    Added solution - LeetHub

---
+++ Matching Pair - GFG/matching-pair.cpp
@@ -0,0 +1,27 @@
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+public:
+    int find(int N){ 
+        // code here
+        return N+1;
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.find(N) << endl;
+    }
+    return 0; 
+}  // } Driver Code Ends
