7316b796e9a4c912ba96940709d2bb57921e720b
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-01-19 11:55:49 +0530

    Added solution - LeetHub

---
+++ Minimum Difference in an Array - GFG/minimum-difference-in-an-array.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    //Function to find minimum difference between any pair of elements in an array.
+    int MinimumDifference(int arr[], int n)
+    {
+        //code here
+        int ans=INT_MAX;
+        sort(arr,arr+n);
+        for(int i=1;i<n;i++){
+        int s = arr[i]-arr[i-1];
+            ans = min(ans,s);
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+    
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    int n; cin>>n;
+	    int arr[n]; 
+	    for(int i = 0; i < n; i++){
+	        cin>>arr[i];
+	    }
+	    Solution ob;
+	    int diff = ob.MinimumDifference(arr, n);
+	    cout << diff << endl;
+	}
+	return 0;
+}  // } Driver Code Ends
