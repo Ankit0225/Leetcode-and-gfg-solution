0c7cbf7e22a765f45a30ca7ecbe5f7302ce92b6f
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-01-21 09:25:45 +0530

    Added solution - LeetHub

---
+++ Maximum money - GFG/maximum-money.cpp
@@ -0,0 +1,30 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    int maximizeMoney(int N , int K) {
+        // code here
+        int sum=0;
+        for(int i=1;i<=N;i+=2)
+        sum+=K;
+        return sum;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,K;
+
+        cin>>N>>K;
+
+        Solution ob;
+        cout << ob.maximizeMoney(N,K) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
