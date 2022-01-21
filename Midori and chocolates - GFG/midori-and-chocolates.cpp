d0515959b148914f3a031ca5a2d1be7a91939539
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-01-21 09:12:08 +0530

    Added solution - LeetHub

---
+++ Midori and chocolates - GFG/midori-and-chocolates.cpp
@@ -0,0 +1,29 @@
+// { Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    long long int leftShops(long long int i, long long int L){
+        // code here
+        int ans=pow(2,L);
+        return (ans-i);
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long int i, L;
+        cin >> i >> L;
+        Solution ob;
+        cout << ob.leftShops(i, L) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
