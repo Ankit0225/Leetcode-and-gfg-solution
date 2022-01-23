2dace4358765967e27c3fa133775fbf0d4368c20
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-01-23 08:33:44 +0530

    Added solution - LeetHub

---
+++ While loop- printTable - GFG/while-loop-print-table.cpp
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    void printTable(int n)
+    {
+        int multiplier=10;
+        while(multiplier && multiplier!=0)
+        {
+           //Your code here
+          int x=n*multiplier;
+          cout<<x<<" ";
+          multiplier--;
+          
+        }
+        cout<<endl;
+        
+    }  
+};
+
+// { Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    Solution obj;
+	    obj.printTable(n);
+	    
+	}
+	return 0;
+}  // } Driver Code Ends
