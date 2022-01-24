ae971fc8fad9898b3b03fa936cf0de2f875657d6
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-01-24 09:14:42 +0530

    Added solution - LeetHub

---
+++ 12. Closet 0s 1s and 2s - GFG/12.-closet-0s-1s-and-2s.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    
+    // The function should do the stated modifications in the given array arr[]
+    // Do not return anything.
+    
+    // arr[]: Input Array
+    // N: Size of the Array arr[]
+    //Function to segregate 0s, 1s and 2s in sorted increasing order.
+    void segragate012(int arr[], int N)
+    {
+        // Your Code Here
+        sort(arr,arr+N);
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+
+    int T;
+    cin >> T;
+
+    while(T--){
+        int N;
+        cin >>N;
+        int A[N];
+        for(int i=0;i<N;i++){
+            cin >> A[i];
+        }
+
+        Solution ob;
+        ob.segragate012(A, N);
+
+        for(int i=0;i<N;i++){
+            cout << A[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+
+}
+  // } Driver Code Ends
