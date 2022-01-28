a082c9110d7bb14d699c735c58985d6cfe0ed8e6
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-01-28 17:19:41 +0530

    Added solution - LeetHub

---
+++ Squares in N*N Chessboard - GFG/squares-in-nn-chessboard.cpp
@@ -0,0 +1,34 @@
+// { Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+  public:
+    long long squaresInChessBoard(long long N) {
+        // code here
+      long long int sum=0;
+        while(N!=0){
+            sum+=N*N;
+            N--;
+        }
+        return sum;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.squaresInChessBoard(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
