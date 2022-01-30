7dd9327d6f81d84cfc160e8802102890bbfa9e7c
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-01-30 23:16:35 +0530

    Added solution - LeetHub

--- 12. Closet 0s 1s and 2s - GFG/12.-closet-0s-1s-and-2s.cpp
+++ 12. Closet 0s 1s and 2s - GFG/12.-closet-0s-1s-and-2s.cpp
@@ -17,7 +17,20 @@ class Solution{
     void segragate012(int arr[], int N)
     {
         // Your Code Here
-        sort(arr,arr+N);
+        int low=0,mid=0,high=N-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr[low],arr[mid]);
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1)
+            mid++;
+            else{
+                swap(arr[mid],arr[high]);
+                high--;
+            }
+        }
     }
 };
 
