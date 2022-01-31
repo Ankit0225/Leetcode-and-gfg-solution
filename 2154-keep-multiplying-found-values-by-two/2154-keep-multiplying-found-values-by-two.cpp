c6383424222e413bfb31aab1b22993c2e3f29a98
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-01-31 18:21:16 +0530

    Time: 3 ms (90.00%), Space: 10.6 MB (10.00%) - LeetHub

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==original)
+                original=2*original;
+        }
+                return original;
+    }
+};
