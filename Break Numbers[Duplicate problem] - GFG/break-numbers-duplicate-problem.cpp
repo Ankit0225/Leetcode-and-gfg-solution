55c1b8dd08e75523031b4b7b3c67bace8cce5ed9
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-02-01 22:54:38 +0530

    Added solution - LeetHub

---
+++ Break Numbers[Duplicate problem] - GFG/break-numbers-duplicate-problem.cpp
@@ -0,0 +1,31 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    long long countWays(long long N){
+        // code here
+        return ((N+2)*(N+1))/2;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.countWays(N)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
