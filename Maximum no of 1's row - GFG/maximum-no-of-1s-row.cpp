0581a86341ca166bfa465f27cdaca7045e49b84a
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-02-03 21:11:12 +0530

    Added solution - LeetHub

---
+++ Maximum no of 1's row - GFG/maximum-no-of-1s-row.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+        int maxOnes (vector <vector <int>> &Mat, int N, int M)
+        {
+            // your code here
+            int rowNo=-1,row=0,col=M-1;
+            while(row<N && col>=0)
+            {
+                if(Mat[row][col]==1)
+                {
+                    rowNo=row;
+                    col--;
+                }
+                else
+                row++;
+            }
+            return rowNo;
+        }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t; cin >> t;
+    while (t--){
+        int n, m; cin >> n >> m;
+        vector <vector <int>> arr (n, vector <int> (m));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> arr[i][j];
+        Solution ob;        
+        cout << ob.maxOnes(arr, n, m) << endl;
+    }
+}  // } Driver Code Ends
