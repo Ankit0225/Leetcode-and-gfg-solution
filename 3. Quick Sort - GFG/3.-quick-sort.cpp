4c82cdb8253ffe68ecc63f99130e2a083ee4faf3
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-02-05 09:00:27 +0530

    Added solution - LeetHub

--- 3. Quick Sort - GFG/3.-quick-sort.cpp
+++ 3. Quick Sort - GFG/3.-quick-sort.cpp
@@ -20,29 +20,30 @@ class Solution
     void quickSort(int arr[], int low, int high)
     {
         // code here
-          if(low<high){
-            int p=partition(arr,low,high);
-            quickSort(arr,low,p-1);
+        if(low<high){
+            int p = partition(arr,low,high);
+            quickSort(arr,low,p);
             quickSort(arr,p+1,high);
         }
-       
     }
     
     public:
     int partition (int arr[], int low, int high)
     {
        // Your code here
-       int pivot = arr[high];
-        int i=low-1;
-        for(int j=low;j<=high-1;j++){
-            if(arr[j]<pivot){
-                i++;
-                swap(arr[i],arr[j]);
-            }
-        }
-            swap(arr[i+1],arr[high]);
-        
-        return (i+1);
+       int pivot=arr[low];
+       int i=low-1,j=high+1;
+       while(true){
+           do{
+               i++;
+           }while(arr[i]<pivot);
+           do{
+               j--;
+           }while(arr[j]>pivot);
+           if(i>=j)
+           return j;
+           swap(arr[i],arr[j]);
+       }
     }
 };
 
