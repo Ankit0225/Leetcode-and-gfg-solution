89b826a07528e7c2ba34e030baa833060a402700
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-02-06 09:23:44 +0530

    Added solution - LeetHub

---
+++ Arranging the array - GFG/arranging-the-array.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void Rearrange(int arr[], int n);
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        cin>>arr[i];
+        long long j=0;
+      
+        Rearrange( arr, n);
+      
+        for (int i = 0; i < n; i++) 
+            cout << arr[i] << " "; 
+        cout << endl;  
+    }
+    return 0; 
+} // } Driver Code Ends
+
+
+
+
+void Rearrange(int arr[], int n)
+{
+    // Your code goes here
+    int temp[n],i=0;
+    for(int j=0;j<n;j++){
+        
+    if(arr[j]<0){
+        temp[i]=arr[j];
+        i++;
+    }
+    }
+    for(int j=0;j<n;j++){
+        if(arr[j]>=0){
+            temp[i]=arr[j];
+            i++;
+        }
+    }
+    for(int j=0;j<n;j++)
+    arr[j]=temp[j];
+}
