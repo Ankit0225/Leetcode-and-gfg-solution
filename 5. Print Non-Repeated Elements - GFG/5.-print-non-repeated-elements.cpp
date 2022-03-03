e0d596a923d0f8b478f128ebb934c1d5b6187ae3
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-03-03 22:22:08 +0530

    Added solution - LeetHub

---
+++ 5. Print Non-Repeated Elements - GFG/5.-print-non-repeated-elements.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+  public:
+    // arr[]: input array
+    // n: size of array
+    //Function to return non-repeated elements in the array.
+    vector<int> printNonRepeated(int arr[],int n)
+    {
+        //Your code here
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++)
+        mp[arr[i]]++;
+        int count=0;
+        vector<int> v;
+        for(int i=0;i<n;i++)
+        if(mp[arr[i]]==1)
+        v.push_back(arr[i]);
+        
+        return v;
+        
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    
+	    vector<int> v;
+	    Solution obj;
+	    v = obj.printNonRepeated(arr,n);
+	    
+	    for(int i=0;i<v.size();i++)  
+	        cout<<v[i]<<" ";
+	    
+	    cout<<endl;
+	    
+	}
+	return 0;
+}
+
+  // } Driver Code Ends
