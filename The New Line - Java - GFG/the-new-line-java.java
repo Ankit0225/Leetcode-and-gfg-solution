439d3251c8d999ad16c31f204df02df7439f4275
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-03-15 23:31:34 +0530

    Added solution - LeetHub

---
+++ The New Line - Java - GFG/the-new-line-java.java
@@ -0,0 +1,30 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+ // } Driver Code Ends
+//User function Template for Java
+
+class Geeks{
+    
+    // Function to print each word in single line
+    static void printIndividualLine(){
+        
+        // Your code here
+        System.out.println("Geeks\nfor\nGeeks");
+    }
+    
+}
+
+// { Driver Code Starts.
+class GfG{
+    
+    public static void main(String args[]){
+        
+        //Creating an object of Class Geeks
+        Geeks g = new Geeks();
+        
+        //Calling a function of Class Geeks
+        g.printIndividualLine();
+    }
+    
+}  // } Driver Code Ends
