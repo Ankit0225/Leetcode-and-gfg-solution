7f0328e7c1cb780533e5a2dd61ca9524d4049802
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-03-19 20:04:37 +0530

    Added solution - LeetHub

---
+++ Sort The Array - GFG/sort-the-array.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+    vector<int> sortArr(vector<int>arr, int n){
+    //complete the function here
+    vector<int> v;
+    for(int i=0;i<n;i++)
+    v.push_back(arr[i]);
+    sort(v.begin(),v.end());
+    return v;
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int>s(n);
+        for(int i = 0; i < n; i++)
+            cin >> s[i];
+        Solution ob;
+        vector<int>v = ob.sortArr(s, n);
+        for(auto i : v)
+            cout << i << ' ';
+        cout << endl;
+    }
+return 0;
+}
+
+  // } Driver Code Ends
