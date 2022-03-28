12d3df2080770a36e9618e12224d455305f54559
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-03-28 23:11:30 +0530

    Added solution - LeetHub

---
+++ C++ STL | Set 8 (unordered set) - GFG/c-stl-set-8-unordered-set.cpp
@@ -0,0 +1,85 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+void insert(unordered_set<int> &s,int x);
+
+void erase(unordered_set<int> &s,int x);
+int size(unordered_set<int> &s);
+int find(unordered_set<int> &s,int x);
+
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		unordered_set<int> s;
+		int q;
+		cin>>q;
+		while(q--)
+		{
+			char c;
+			cin>>c;
+			if(c=='a')
+			{
+				int x;
+				cin>>x;
+				insert(s,x);
+			}
+		
+			if(c=='b')
+			{
+				int x;
+				cin>>x;
+				erase(s,x);
+			}
+			if(c=='c')
+			{
+				int x;
+				cin>>x;
+				cout<<find(s,x)<<" ";
+			}if(c=='d')
+                       cout<<size(s)<<" ";
+		
+		}
+cout<<endl;
+	}
+	return 0;
+}// } Driver Code Ends
+
+
+/* You are required to complete below methods */
+
+/*inserts an element x to the unordered set s */
+void insert(unordered_set<int> &s,int x)
+{
+	//Your code here
+	s.insert(x);
+}
+
+
+/*erases an element x from the unordered set s */
+void erase(unordered_set<int> &s,int x)
+{
+	//Your code here
+	s.erase(x);
+}
+
+/*returns the size of the unordered set s */
+int size(unordered_set<int> &s)
+{
+      //Your code here
+      return s.size();
+}
+
+/*returns 1 if the element x is present
+in unordered set s else returns -1 */
+int find(unordered_set<int> &s,int x)
+{
+     //Your code here
+     if(s.find(x)!=s.end())
+     return 1;
+     return -1;
+}
