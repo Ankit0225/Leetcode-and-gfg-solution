9a6c52cc1a16f92a8dcbd2578f684eee12d7f22d
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-05-06 10:03:34 +0530

    Added solution - LeetHub

--- Reverse a string using Stack - GFG/reverse-a-string-using-stack.cpp
+++ Reverse a string using Stack - GFG/reverse-a-string-using-stack.cpp
@@ -28,6 +28,16 @@ int main()
 char* reverse(char *S, int len)
 {
     //code here
-    reverse(S,S+len);
+    
+    stack<char> s;
+    for(int i=0;i<len;i++)
+    s.push(S[i]);
+    int i=0;
+    while(!s.empty())
+    {
+    S[i++] = s.top();
+    
+    s.pop();    
+    }
     return S;
 }
