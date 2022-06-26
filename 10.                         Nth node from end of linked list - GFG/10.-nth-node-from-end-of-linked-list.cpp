c5ac27ea84777ece1d49ea7bec674c02fdf13f9a
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-06-26 10:00:53 +0530

    Added solution - LeetHub

---
+++ 10.                         Nth node from end of linked list - GFG/10.-nth-node-from-end-of-linked-list.cpp
@@ -0,0 +1,82 @@
+// { Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+/* Function to get the nth node from the last of a linked list*/
+int getNthFromLast(struct Node* head, int n);
+
+
+
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,k;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n>>k;
+        int firstdata;
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+
+    cout<<getNthFromLast(head, k)<<endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to find the data of nth node from the end of a linked list.
+int getNthFromLast(Node *head, int n)
+{
+       // Your code here
+       Node *first=head,*second=head;
+       if(head==NULL)
+       return -1;
+       for(int i=0;i<n;i++)
+       {
+           if(first==NULL)
+           return -1;
+           first=first->next;
+       }
+       while(first!=NULL)
+       {
+           second=second->next;
+           first=first->next;
+       }
+       return second->data;
+}
+
