e0e1e80337d22d7a9d88a05b76b858961dc04e64
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-07-15 20:29:54 +0530

    Added solution - LeetHub

--- Min sum formed by digits - GFG/min-sum-formed-by-digits.cpp
+++ Min sum formed by digits - GFG/min-sum-formed-by-digits.cpp
@@ -9,24 +9,13 @@ class Solution{
     long long int minSum(int arr[], int n)
     {
         // Your code goes here
+        long long int sum1=0,sum2=0;
         sort(arr,arr+n);
-        priority_queue<int,vector<int>,greater<int>> minH1,minH2;
         for(int i=0;i<n;i+=2)
-        minH1.push(arr[i]);
+           sum1=sum1*10+arr[i];
         for(int i=1;i<n;i+=2)
-        minH2.push(arr[i]);
-        long long int sum1= 0;
-        long long int sum2= 0;
-        while(minH1.size()>0){
-        sum1= sum1*10 +minH1.top();
-        minH1.pop();    
-        }
-        while(minH2.size()>0)
-        {
-        sum2= sum2*10+minH2.top();
-        minH2.pop();    
-        }
-        return sum1+sum2;
+           sum2=sum2*10+arr[i];
+       return sum1+ sum2;
     }
     
 };
