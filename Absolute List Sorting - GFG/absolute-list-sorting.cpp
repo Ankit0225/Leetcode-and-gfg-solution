eeaa3d07e4f5e86d9654e61de0ff1c9c494c1c2d
Ankit Maurya <ankitmaurya0225@gmail.com>
2022-12-21 08:18:54 +0530

    Added solution - LeetHub

---
+++ Absolute List Sorting - GFG/absolute-list-sorting.cpp
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Linked List Node
+struct Node
+{
+	Node* next;
+	int data;
+};
+
+// Utility function to insert a node at the
+// beginning
+void push(Node** head, int data)
+{
+	Node* newNode = new Node;
+	newNode->next = (*head);
+	newNode->data = data;
+	(*head) = newNode;
+}
+
+// Utility function to print a linked list
+void printList(Node* head)
+{
+	while (head != NULL)
+	{
+		cout << head->data;
+		if (head->next != NULL)
+			cout << " ";
+		head = head->next;
+	}
+	cout << endl;
+}
+
+
+// } Driver Code Ends
+/* The structure of the Linked list Node is as follows:
+struct Node
+{
+    Node* next;
+    int data;
+}; */
+
+/*Your method shouldn't print anything
+ it should transform the passed linked list */
+class Solution{
+    
+public:
+    Node* sortList(Node* head)
+    {
+        // Your Code Here
+        vector<int> v;
+        Node *curr=head;
+        while(curr!=NULL)
+        {
+            v.push_back(curr->data);
+            curr=curr->next;
+        }
+        sort(v.begin(),v.end());
+        curr=head;
+        // for(int i=0;i<v.size();i++)
+        // {
+        //     cout<<v[i]<<" ";
+        // }
+        // cout<<endl;
+        for(int i=0;i<v.size();i++)
+        {
+            curr->data=v[i];
+            curr=curr->next;
+        }
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+
+	int t = 0;
+	int n = 0;
+	cin >> t;
+	while (t--)
+	{
+		Node* head = NULL;
+		cin >> n;
+		int arr[n];
+		for (int i = 0; i < n; i++)
+		{
+			cin >> arr[i];
+			// push(&head, a);
+		}
+		for (int i = n - 1; i >= 0; i--)
+		{
+			push(&head, arr[i]);
+		}
+		// printList(head);
+		
+		Solution ob;
+		head=ob.sortList(head);
+
+		printList(head);
+
+	}
+	return 0;
+}
+
+// } Driver Code Ends
