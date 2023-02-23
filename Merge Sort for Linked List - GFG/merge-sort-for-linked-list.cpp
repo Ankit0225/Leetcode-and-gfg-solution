8b34f48ccfff74f800ef8100154d2e45773e8483
Ankit Maurya <ankitmaurya0225@gmail.com>
2023-02-23 20:44:31 +0530

    Added solution - LeetHub

---
+++ Merge Sort for Linked List - GFG/merge-sort-for-linked-list.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/* Structure of the linked list node is as
+struct Node 
+{
+    int data;
+    struct Node* next;
+    Node(int x) { data = x;  next = NULL; }
+};
+*/
+
+
+class Solution{
+  public:
+    //Function to sort the given linked list using Merge Sort.
+    Node* mergeSort(Node* head) {
+        // your code here
+        Node *curr=head;
+        vector<int> ans;
+        while(curr!=NULL)
+        {
+            ans.push_back(curr->data);
+            curr=curr->next;
+        }
+        sort(ans.begin(),ans.end());
+        Node* head1=NULL;
+        Node* tail=NULL;
+        for(int i=0;i<ans.size();i++)
+        {
+            if(head1==NULL)
+            {
+                head1=tail= new Node(ans[i]);
+            }
+            else
+            {
+                tail->next= new Node(ans[i]);
+                tail=tail->next;
+            }
+        }
+        tail->next=NULL;
+        return head1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+void printList(Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+void push(struct Node** head_ref, int new_data) {
+    Node* new_node = new Node(new_data);
+
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+int main() {
+    long test;
+    cin >> test;
+    while (test--) {
+        struct Node* a = NULL;
+        long n, tmp;
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            cin >> tmp;
+            push(&a, tmp);
+        }
+        Solution obj;
+        a = obj.mergeSort(a);
+        printList(a);
+    }
+    return 0;
+}
+// } Driver Code Ends
