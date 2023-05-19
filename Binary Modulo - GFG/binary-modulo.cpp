3ddce9b1d48c1f593c2c1cc464f7941847d67cde
Ankit Maurya <ankitmaurya0225@gmail.com>
2023-05-19 22:47:44 +0530

    Added solution - LeetHub

---
+++ Binary Modulo - GFG/binary-modulo.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+        int modulo(string s,int m)
+        {
+            //code here
+            long decimal=0;
+            for(int i=0;i<s.length();i++)
+            {
+                decimal=(decimal*2+s[i]-'0')%m;
+            }
+            return decimal%m;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        int m;
+        cin>>s>>m;
+        Solution a;
+        cout<<a.modulo(s,m)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
