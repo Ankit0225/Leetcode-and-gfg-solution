868f6bd0ff6d585ac4e0e350de36ba9e0a1263a1
Ankit Maurya <ankitmaurya0225@gmail.com>
2023-06-25 15:16:39 +0530

    Added solution - LeetHub

--- Find Kth permutation - GFG/find-kth-permutation.cpp
+++ Find Kth permutation - GFG/find-kth-permutation.cpp
@@ -7,30 +7,30 @@ using namespace std;
 
 // } Driver Code Ends
 //User function Template for C++
-
+# define pb push_back
 class Solution
 {
 public:
     string kthPermutation(int n, int k)
     {
         // code here
-        vector<int> nums;
         int fact=1;
+        vector<int> nums;
         for(int i=1;i<n;i++)
         {
+            nums.pb(i);
             fact*=i;
-            nums.push_back(i);
         }
-        nums.push_back(n);
-        k--;
+        nums.pb(n);
         string ans="";
-        while(1)
+        k--;
+        while(true)
         {
             ans+=to_string(nums[k/fact]);
-            nums.erase(nums.begin()+ k/fact);   
+            nums.erase(nums.begin()+k/fact);
             if(nums.size()==0)
             break;
-            k= k % fact;
+            k=k%fact;
             fact/=nums.size();
         }
         return ans;
