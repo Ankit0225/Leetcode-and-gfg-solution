52a7092fca06998bc959f230bf07430bfb4bc442
Ankit Maurya <ankitmaurya0225@gmail.com>
2023-08-09 21:01:22 +0530

    Added solution - LeetHub

--- Shortest Distance in a Binary Maze - GFG/shortest-distance-in-a-binary-maze.cpp
+++ Shortest Distance in a Binary Maze - GFG/shortest-distance-in-a-binary-maze.cpp
@@ -13,19 +13,16 @@ class Solution {
     int shortestPath(vector<vector<int>> &grid, pair<int, int> source,
                      pair<int, int> destination) {
         // code here
-        int n = grid.size();
-        int m = grid[0].size();
+        int n = grid.size(), m= grid[0].size();
         int u = source.first;
         int v = source.second;
         int x = destination.first;
         int y = destination.second;
-        if(grid[u][v] == 0 || grid[u][v] == 0)
-        return -1;
+        queue<pair<int,pair<int,int>>>  q;
+        q.push({0,{u,v}});
         
-        vector<vector<bool>> visited(n,vector<bool>(m,false));
-        queue<pair<pair<int,int>,int>> q;
-        visited[u][v] = true;
-        q.push({{u,v},0});
+        vector<vector<int>> dist(n,vector<int>(m,1e9));
+        dist[u][v] = 0;
         
         vector<int> delrow = {-1,0,1,0};
         vector<int> delcol = {0,1,0,-1};
@@ -34,31 +31,28 @@ class Solution {
             auto p = q.front();
             q.pop();
             
-            int row = p.first.first;
-            int col = p.first.second;
-            int counter = p.second;
+            int dis = p.first;
+            int row = p.second.first;
+            int col = p.second.second;
             
             if(row == x && col == y)
-            return counter;
-            
+            return dis;
             for(int i=0;i<4;i++)
             {
-                int nrow =  row + delrow[i];
+                int nrow = row + delrow[i];
                 int ncol = col + delcol[i];
                 
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m
-                && visited[nrow][ncol] == false && grid[nrow][ncol] == 1)
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol] == 1)
                 {
-                    visited[nrow][ncol] = true;
-                    q.push({{nrow,ncol},counter+1});
+                    if(dis + 1 < dist[nrow][ncol])
+                    {
+                        dist[nrow][ncol] = dis + 1;
+                        q.push({dis + 1,{nrow,ncol}});
+                    }
                 }
             }
         }
-        
         return -1;
-       
-        
-        
     }
 };
 
