32ca648fac7cd19964182b7873805dc66651bc27
Ankit Maurya <ankitmaurya0225@gmail.com>
2023-08-09 10:13:05 +0530

    Added solution - LeetHub

---
+++ Largest prime factor - GFG/largest-prime-factor.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public: 
+    long long int largestPrimeFactor(int N){
+        // code here
+        long long int ans = 2;
+        while((ans*ans)<=N){
+            if(N%ans == 0){
+                N = N/ans;
+            }
+            else{
+                ans++;
+            }
+        }
+        return N;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.largestPrimeFactor(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
