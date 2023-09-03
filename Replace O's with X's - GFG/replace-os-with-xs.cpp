a6c03d6cdd7da52248dc745849ca866d80d0ed09
Ankit Maurya <ankitmaurya0225@gmail.com>
2023-09-04 00:19:43 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.cpp
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -9,62 +9,76 @@ using namespace std;
 
 class Solution{
 public:
-    void dfs(int row,int col,vector<vector<char>> &mat,vector<vector<bool>> &vis)
+    vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
     {
-        vis[row][col] = true;
-        int n=mat.size(),m=mat[0].size();
+        // code here
+        vector<vector<bool>> visited(n,vector<bool>(m,false));
+        
+        queue<pair<int,int>> q;
+        // traverse row
+        
+        for(int j=0;j<m;j++)
+        {
+            if(mat[0][j] == 'O' && visited[0][j] == false)
+            {
+                q.push({0,j});
+                visited[0][j] = true;
+            }
+            
+            if(mat[n-1][j] == 'O' && visited[n-1][j] == false)
+            {
+                q.push({n-1,j});
+                visited[n-1][j] = true;
+            }
+        }
+        
+        // traverse column;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(mat[i][0] == 'O' && visited[i][0] == false)
+            {
+                q.push({i,0});
+                visited[i][0] = true;
+            }
+            
+            if(mat[i][m-1] == 'O' && visited[i][m-1] == false)
+            {
+                q.push({i,m-1});
+                visited[i][m-1] = true;
+            }
+        }
         
         vector<int> delrow = {-1,0,1,0};
         vector<int> delcol = {0,1,0,-1};
         
-        for(int i=0;i<4;i++)
-        {
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-            
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol < m 
-            && vis[nrow][ncol]==false && mat[nrow][ncol]=='O')
-            {
-                dfs(nrow,ncol,mat,vis);
-            }
-        }
-    }
-    vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
-    {
-        // code here
-        vector<vector<bool>> vis(n,vector<bool>(m,false));
-        for(int i=0;i<n;i++)
-        {
-            if(vis[i][0]==false && mat[i][0]=='O')
-            {
-                dfs(i,0,mat,vis);
-            }
-            if(vis[i][m-1]==false && mat[i][m-1]=='O')
-            {
-                dfs(i,m-1,mat,vis);
-            }
-        }
-        for(int j=0;j<m;j++)
-        {
-            if(vis[0][j]==false && mat[0][j]=='O')
-            {
-                dfs(0,j,mat,vis);
-            }
-            if(vis[n-1][j]==false && mat[n-1][j]=='O')
-            {
-                dfs(n-1,j,mat,vis);
-            }
-        }
         
+        while(!q.empty())
+        {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            
+            for(int i=0;i<4;i++)
+            {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && 
+                visited[nrow][ncol] == false && mat[nrow][ncol] == 'O')
+                {
+                    visited[nrow][ncol]= true;
+                    q.push({nrow,ncol});
+                }
+            }
+        }
         
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(vis[i][j]==false && mat[i][j]=='O')
-                {
-                    mat[i][j]='X';
-                }
+                if(visited[i][j] == false && mat[i][j] == 'O')
+                mat[i][j] = 'X';
             }
         }
         return mat;
