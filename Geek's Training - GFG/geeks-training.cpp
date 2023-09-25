8515de105124e5b7ea72e6756b78e116c3d44269
Ankit Maurya <ankitmaurya0225@gmail.com>
2023-09-25 15:57:47 +0530

    Added solution - LeetHub

--- Geek's Training - GFG/geeks-training.cpp
+++ Geek's Training - GFG/geeks-training.cpp
@@ -7,35 +7,31 @@ class Solution {
   public:
     int maximumPoints(vector<vector<int>>& points, int n) {
         // Code here
-        vector<vector<int>> dp(n,vector<int>(4,0));
+        vector<int> dp(4,0);
         
-        dp[0][0] = max(points[0][1],points[0][2]);
-        dp[0][1] = max(points[0][2],points[0][0]);
-        dp[0][2] = max(points[0][0],points[0][1]);
-        
-        dp[0][3] = max(points[0][0],max(points[0][1],points[0][2]));
-        
+        dp[0] = max(points[0][1],points[0][2]);
+        dp[1] = max(points[0][0],points[0][2]);
+        dp[2] = max(points[0][1],points[0][0]);
+        dp[3] = max(points[0][0],max(points[0][1],points[0][2]));
         
         for(int day=1;day<n;day++)
         {
-            for(int last = 0;last<4;last++)
+            vector<int> temp(4,0);
+            for(int last =0;last<4;last++)
             {
-                dp[day][last] = 0;
-                
-                for(int task=0;task<3;task++)
+                temp[last] = 0;
+                for(int task = 0;task<3;task++)
                 {
-                    if(task!=last)
-                    {
-                        int point = points[day][task] + dp[day-1][task];
-                        
-                        dp[day][last] = max(dp[day][last],point);
-                    }
+                    if(task != last)
+                    temp[last]= max(temp[last],points[day][task] + dp[task]);
                 }
             }
+            
+            dp = temp;
         }
-        
-        return dp[n-1][3];
+        return dp[3];
     }
+    
 };
 
 //{ Driver Code Starts.
