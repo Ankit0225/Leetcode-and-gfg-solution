ee3c791828a87c1540c16e07ec311226dba56ba2
Ankit Maurya <ankitmaurya0225@gmail.com>
2023-10-13 13:49:05 +0530

    Added solution - LeetHub

--- Floor in BST - GFG/floor-in-bst.cpp
+++ Floor in BST - GFG/floor-in-bst.cpp
@@ -15,7 +15,34 @@ struct Node {
     }
 };
 
-int floor(Node *root, int x);
+
+// } Driver Code Ends
+// Function to search a node in BST.
+class Solution{
+
+public:
+    int floor(Node* root, int x) {
+        // Code here
+        Node *res = NULL;
+        while(root!=NULL)
+        {
+            if(root->data == x)
+                return root->data;
+            else if(root->data > x)
+                root = root->left;
+            else
+            {
+                res = root;
+                root = root->right;
+            }
+        }
+        if(res == NULL)
+            return -1;
+        return res->data;
+    }
+};
+
+//{ Driver Code Starts.
 
 Node *insert(Node *tree, int val) {
     Node *temp = NULL;
@@ -46,32 +73,9 @@ int main() {
 
         int s;
         cin >> s;
-
-        cout << floor(root, s) << "\n";
+        Solution obj;
+        cout << obj.floor(root, s) << "\n";
     }
 }
 
 // } Driver Code Ends
-
-
-// Function to search a node in BST.
-
-int floor(Node* root, int x) {
-    // Code here
-    int res=-1;
-    while(root!=NULL)
-    {
-        if(root->data==x)
-        return x;
-        else if(root->data>x)
-        {
-            root=root->left;
-        }
-        else
-        {
-            res=root->data;
-            root=root->right;
-        }
-    }
-    return res;
-}
