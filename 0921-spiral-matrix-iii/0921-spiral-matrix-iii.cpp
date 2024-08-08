00cf5632e09e8eabaaff23120b5cfeb5cf5d4fc6
Ankit Maurya <ankitmaurya0225@gmail.com>
2024-08-08 23:04:55 +0530

    Time: 11 ms (42.57%), Space: 13.9 MB (23.17%) - LeetHub

--- 0921-spiral-matrix-iii/0921-spiral-matrix-iii.cpp
+++ 0921-spiral-matrix-iii/0921-spiral-matrix-iii.cpp
@@ -4,16 +4,15 @@ public:
         vector<vector<int>> ans;
         vector<int> delrow = {0,1,0,-1},delcol = {1,0,-1,0};
         int r = rStart,c =cStart,steps = 1,dir = 0;
-        ans.push_back({r,c});
+        
         while( ans.size() < rows * cols)
         {
             for (int i = 0; i < 2; i++) {
                 for (int j = 0; j < steps; j++) {
+                    if (r >= 0 && r < rows && c >= 0 && c < cols)
+                        ans.push_back({r, c});
                     r += delrow[dir];
                     c += delcol[dir];
-                    if (r >= 0 && r < rows && c >= 0 && c < cols) {
-                        ans.push_back({r, c});
-                    }
                 }
                 dir = (dir + 1) % 4;
             }
